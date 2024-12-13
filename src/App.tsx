54551745e2451209b4e6eb465cb16e730a67b926
Matteo <matteonegridev@gmail.com>
2024-12-13 20:31:33 +0100

    update gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import FormApp from "./components/Form/FormApp";
 import ResultSection from "./components/ResultSection/ResultSection";
-import SelectCurrency from "./components/SelectCurrency/SelectCurrency";
 import { ContextProvider } from "./utils/Context";
 
 function App() {
   return (
     <div>
       <ContextProvider>
-        <SelectCurrency />
         <FormApp />
         <ResultSection />
       </ContextProvider>
--- src/components/SelectCurrency/SelectCurrency.tsx
+++ src/components/SelectCurrency/SelectCurrency.tsx
@@ -5,29 +5,44 @@ import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import useCurrenciesHook from "../../api/useCurrencies";
 import { useState } from "react";
+import "./SelectCurrency.scss";
+import fx from "money";
 
 export default function SelectCurrency() {
   const [currency, setCurrency] = useState("");
   const { data } = useCurrenciesHook();
 
   const handleChange = (event: SelectChangeEvent) => {
-    setCurrency(event.target.value as string);
+    const selectedCurrency = event.target.value as string;
+    setCurrency(selectedCurrency);
+
+    if (data && data.rates) {
+      fx.rates = data.rates;
+      fx.base = selectedCurrency;
+      console.log(
+        "Updated money.js with rates for base currency:",
+        selectedCurrency
+      );
+    }
   };
 
   return (
     <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Currency</InputLabel>
+      <FormControl className="form-control">
+        <InputLabel className="select-label" id="demo-simple-select-label">
+          Currency
+        </InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
+          className="select-label select-label--code"
           value={currency}
           label="Currency"
           onChange={handleChange}
         >
-          <MenuItem value={10}>USD</MenuItem>
-          <MenuItem value={20}>GBP</MenuItem>
-          <MenuItem value={30}>EUR</MenuItem>
+          <MenuItem value="USD">USD</MenuItem>
+          <MenuItem value="GBP">GBP</MenuItem>
+          <MenuItem value="EUR">EUR</MenuItem>
         </Select>
       </FormControl>
     </Box>
